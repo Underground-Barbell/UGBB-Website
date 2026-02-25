4db62b3e80e30acfbbebcf93b02ccbd5a8e92734
Caden Epp <ccepp2412@gmail.com>
2026-02-25 07:43:23 -0600

    introduction html

--- app/sections/introduction.tsx
+++ app/sections/introduction.tsx
@@ -6,7 +6,11 @@ export default function Introduction(){
 
     return (
         <section id="Introduction">
-            <h1>Introduction Section</h1>
+            <h2>WELCOME</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac ipsum  gravida, vestibulum
+                diam ut, vehicula velit. Integer augue eros,  porttitor et imperdiet non, dignissim
+                posuere massa. Aliquam suscipit  pretium eros, eget venenatis erat suscipit ut.
+            </p>
         </section>
     );
 }
