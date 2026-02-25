6ee1a832d05de10391f63f042e82f81c30b8d384
Caden Epp <ccepp2412@gmail.com>
2026-02-25 14:31:37 -0600

    locations html

--- app/sections/locations.tsx
+++ app/sections/locations.tsx
@@ -1,12 +1,22 @@
 'use client'
 import { useState } from 'react';
+import LocationCard from "@/app/components/LocationCard";
 
 
 export default function Locations(){
 
     return (
         <section id="Locations">
-            <h1>Locations Section</h1>
+            <div id="APIMapContainer">
+                {/* TODO: Integrate API Map Here */}
+            </div>
+            <div id="LocationsContainer">
+                <h1>LOCATIONS</h1>
+                <div id="LocationCardContainer">
+                    <LocationCard title="OTTUMWA" address="123 Address Street" city="Ottumwa" state="Iowa" availability="24/7" />
+                    <LocationCard title="HEDRICK" address="123 Address Street" city="Hedrick" state="Iowa" availability="24/7" />
+                </div>
+            </div>
         </section>
     );
 }
