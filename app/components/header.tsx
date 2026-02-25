d1a269610175eb9c96fc54a8bb837dfbe274fbae
Caden Epp <ccepp2412@gmail.com>
2026-02-25 07:27:13 -0600

    html for header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,11 +1,28 @@
 'use client'
 import { useState } from 'react';
+import Image from 'next/image'
+import Link from 'next/link'
+import UGBB from "../../public/LOGO/UGBB.png"
+import MENU from "../../public/Icons/menu.svg"
 
 export default function Header(){
 
     return (
         <header id="Header">
-            <h1>Header</h1>
+            <div id="LogoContainer">
+                <Image src={UGBB} alt="UGBB Logo" width={50} height={50} />
+            </div>
+            <nav id="NavContainer">
+                <Link href="/" >ABOUT</Link>
+                <Link href="/" >MEMBERSHIPS</Link>
+                <Link href="/" >EQUIPMENT</Link>
+                <Link href="/" >LOCATIONS</Link>
+                <Link href="/" >CLASSES/EVENTS</Link>
+                <Link href="/" >CONTACT</Link>
+            </nav>
+            <div id="MenuContainer">
+                <Image src={MENU} alt="Menu Button" width={50} height={50} />
+            </div>
         </header>
     );
 }
