843e297d30b8bf21baacef2ed026d97b19699466
Caden Epp <ccepp2412@gmail.com>
2026-03-03 14:54:28 -0600

    static styles for header and started hero posistioning

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,24 +4,43 @@ import Image from 'next/image'
 import Link from 'next/link'
 import UGBB from "../../public/LOGO/UGBB.png"
 import MENU from "../../public/Icons/menu.svg"
+import { Menu } from 'lucide-react';
 
 export default function Header(){
 
     return (
-        <header id="Header">
-            <div id="LogoContainer">
-                <Image src={UGBB} alt="UGBB Logo" width={50} height={50} />
+        <header
+            id="Header"
+            className="max-h-screen w-full flex justify-between items-center mt-7 fixed top-0"
+        >
+            <div
+                id="LogoContainer"
+                className="relative left-8"
+            >
+                <Image src={UGBB} alt="UGBB Logo" width={65} height={65} />
             </div>
-            <nav id="NavContainer">
-                <Link href="/" >ABOUT</Link>
-                <Link href="/" >MEMBERSHIPS</Link>
-                <Link href="/" >EQUIPMENT</Link>
-                <Link href="/" >LOCATIONS</Link>
-                <Link href="/" >CLASSES/EVENTS</Link>
-                <Link href="/" >CONTACT</Link>
-            </nav>
-            <div id="MenuContainer">
-                <Image src={MENU} alt="Menu Button" width={50} height={50} />
+            <div
+                className="flex justify-end w-3/4 min-w-3/4"
+            >
+                <nav
+                    id="NavContainer"
+                    className="bg-black text-white border-4 border-white w-full flex items-center justify-between px-10 font-dosis font-extrabold text-2xl relative right-8"
+                >
+                    <Link href="/" >ABOUT</Link>
+                    <Link href="/" >MEMBERSHIPS</Link>
+                    <Link href="/" >EQUIPMENT</Link>
+                    <Link href="/" >LOCATIONS</Link>
+                    <Link href="/" >CLASSES/EVENTS</Link>
+                    <Link href="/" >CONTACT</Link>
+                </nav>
+                <div
+                    id="MenuContainer"
+                    className="bg-black border-white border-4 w-[65px] h-[65px] text-white flex items-center justify-center ml-8 relative right-8"
+                >
+                    <Menu
+                        className="h-[50px] w-[50px] "
+                    />
+                </div>
             </div>
         </header>
     );
--- app/sections/hero.tsx
+++ app/sections/hero.tsx
@@ -5,14 +5,23 @@ import { useState } from 'react';
 export default function Hero(){
 
     return (
-        <section id="Hero">
-            <div id="HeroContainer">
+        <section
+            id="Hero"
+            className="bg-[url(/testing-bg-img-hero.png)] h-screen"
+        >
+            <div
+                id="HeroContainer"
+                className="flex justify-center items-center h-full"
+            >
                 {/* TODO: Cinematic Video Here */}
-                <div id="TitleContainer">
-                    <h1>UNDERGROUND</h1>
-                    <h1>BARBELL</h1>
-                    <hr />
-                    <h3>BE THE BEST - WORK HARD</h3>
+                <div
+                    id="TitleContainer"
+                    className="flex flex-col justify-center items-center text-ugbb-red-main font-sans-bogle text-9xl"
+                >
+                    <h1 className="text-border">UNDERGROUND</h1>
+                    <h1 className="text-border">BARBELL</h1>
+                    <hr className="border-6 border-white rounded-full my-2 w-3/4"/>
+                    <h3 className="text-2xl font-sans-bartle text-border">BE THE BEST - WORK HARD</h3>
                 </div>
             </div>
         </section>
