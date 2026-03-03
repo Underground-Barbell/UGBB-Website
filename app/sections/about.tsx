a7b5da7f13cd9d18d6017776cebd12822935ffeb
Caden Epp <ccepp2412@gmail.com>
2026-03-03 16:10:57 -0600

    static styles for about

--- app/sections/about.tsx
+++ app/sections/about.tsx
@@ -9,22 +9,25 @@ import PersonalTraining from "../../public/AboutImages/personalTrainingAboutUGBB
 export default function About(){
 
     return (
-        <section id="About">
-            <div id="AboutInfoContainer">
-                <h2>ABOUT</h2>
-                <h2>UGBB</h2>
-                <hr />
-                <hr />
-                <p>
+        <section id="About"
+            className="bg-black text-white pb-24 border-b-28 border-ugbb-red-sec"
+        >
+            <div
+                id="AboutInfoContainer"
+                className="relative left-12"
+            >
+                <h2 className="font-sans-bartle text-5xl text-border-red">ABOUT</h2>
+                <h2 className="font-sans-bartle text-5xl text-border-red">UGBB</h2>
+                <hr className="border-3 w-md rounded-full text-ugbb-red-main mt-2" />
+                <p className="font-dosis text-lg w-3/8 mt-2 font-bold">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac ipsum
-                    gravida, vestibulum diam ut, vehicula velit. Integer augue eros,  porttitor
-                    et imperdiet non, dignissim posuere massa. Aliquam suscipit  pretium eros, eget
-                    venenatis erat suscipit ut. Lorem ipsum dolor sit amet, consectetur adipiscing
-                    elit. Nam ac ipsum  gravida, vestibulum diam ut, vehicula velit. Integer augue
-                    eros,  porttitor et imperdiet non, dignissim posuere massa.
+                    gravida, vestibulum diam ut, vehicula velit.
                 </p>
             </div>
-            <div id="AboutCardContainer">
+            <div
+                id="AboutCardContainer"
+                className="flex justify-center items-center mt-8"
+            >
                 <AboutCard imageData={BicepCurlAndLegExtension} title="PROFESSIONAL EQUIPMENT" />
                 <AboutCard imageData={Community} title="SUPPORTIVE COMMUNITY" />
                 <AboutCard imageData={PersonalTraining} title="PERSONAL TRAINING" />
