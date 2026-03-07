60804ae07979fb3d7ff3f38982ea1b615094e31f
Caden Epp <ccepp2412@gmail.com>
2026-03-06 19:37:56 -0600

    Static Styles for Classes

--- app/sections/classes.tsx
+++ app/sections/classes.tsx
@@ -1,12 +1,35 @@
 'use client'
 import { useState } from 'react';
+import ClassesCard from "@/app/components/ClassesCard";
 
 
 export default function Classes(){
 
     return (
-        <section id="Classes">
-            <h1>Classes Section</h1>
+        <section
+            id="Classes"
+            className="bg-black border-b-28 border-ugbb-red-sec"
+        >
+            <div
+                id="ClassesAndEventsContainer"
+                className="flex w-full"
+            >
+                <div
+                    id="ClassesContainer"
+                    className="m-25 w-1/2"
+                >
+                    <ClassesCard title={"CLASSES"} />
+                </div>
+
+                <div
+                    id="EventsContainer"
+                    className="m-25 w-1/2"
+                >
+                    <ClassesCard title={"EVENTS"} />
+                </div>
+
+            </div>
+
         </section>
     );
 }
