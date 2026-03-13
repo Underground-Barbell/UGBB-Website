d2d68dd957dc9a8cc7c2c73e60ce22110a79e048
Caden Epp <ccepp2412@gmail.com>
2026-03-13 09:10:48 -0500

    responsive styles for hero and fixed header and footer responsive styles

--- app/sections/hero.tsx
+++ app/sections/hero.tsx
@@ -16,12 +16,12 @@ export default function Hero(){
                 {/* TODO: Cinematic Video Here */}
                 <div
                     id="TitleContainer"
-                    className="flex flex-col justify-center items-center text-ugbb-red-main font-sans-bogle text-9xl"
+                    className="flex flex-col justify-center items-center text-ugbb-red-main font-sans-bogle text-7xl lg:text-9xl"
                 >
                     <h1 className="text-border">UNDERGROUND</h1>
                     <h1 className="text-border">BARBELL</h1>
                     <hr className="border-6 border-white rounded-full my-2 w-3/4"/>
-                    <h3 className="text-2xl font-sans-bartle text-border">BE THE BEST - WORK HARD</h3>
+                    <h3 className="text-[15px] lg:text-2xl font-sans-bartle text-border">BE THE BEST - WORK HARD</h3>
                 </div>
             </div>
         </section>
