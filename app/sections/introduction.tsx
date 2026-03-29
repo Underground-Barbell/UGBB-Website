7704d485e84f34b16b0cfb775e6d69936daef2be
Caden Epp <ccepp2412@gmail.com>
2026-03-28 19:38:04 -0500

    Introduction and About Title t&e

--- app/sections/introduction.tsx
+++ app/sections/introduction.tsx
@@ -1,24 +1,28 @@
 'use client'
-import { useState } from 'react';
+import {useState, useEffect, useRef} from 'react';
+import AnimationOnScroll from "@/app/tools/AnimationOnScroll";
 
 
 export default function Introduction(){
+
 
     return (
         <section
             id="Introduction"
             className="border-t-20 md:border-t-28 border-ugbb-red-sec text-white bg-black "
         >
-            <div
-                className="flex flex-col justify-center items-center md:items-end md:text-end"
-            >
-                <h2 className="text-4xl md:text-5xl font-sans-bartle text-border-red mt-8 md:mx-8">WELCOME</h2>
-                <hr className="border-3 w-85 md:w-130 lg:w-165 rounded-full text-ugbb-red-main mt-2 md:mx-8"/>
-                <p className="font-dosis text-[17px] md:text-lg md:w-1/2 mx-8 mt-2 font-bold">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac ipsum  gravida, vestibulum
-                    diam ut, vehicula velit. Integer augue eros,  porttitor et imperdiet non, dignissim
-                    posuere massa. Aliquam suscipit  pretium eros, eget venenatis erat suscipit ut.
-                </p>
-            </div>
+            <AnimationOnScroll classNameInView={"animate-slide-in-right opacity-100"} classNameNotInView={"opacity-0"}>
+                <div
+                    className="flex flex-col justify-center items-center md:items-end md:text-end"
+                >
+                    <h2 className="text-4xl md:text-5xl font-sans-bartle text-border-red mt-8 md:mx-8">WELCOME</h2>
+                    <hr className="border-3 w-85 md:w-130 lg:w-165 rounded-full text-ugbb-red-main mt-2 md:mx-8"/>
+                    <p className="font-dosis text-[17px] md:text-lg md:w-1/2 mx-8 mt-2 font-bold">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac ipsum  gravida, vestibulum
+                        diam ut, vehicula velit. Integer augue eros,  porttitor et imperdiet non, dignissim
+                        posuere massa. Aliquam suscipit  pretium eros, eget venenatis erat suscipit ut.
+                    </p>
+                </div>
+            </AnimationOnScroll>
         </section>
     );
 }
