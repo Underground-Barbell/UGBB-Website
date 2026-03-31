da931eeedbb3abe1bedb58562283308b812db5f3
Caden Epp <ccepp2412@gmail.com>
2026-03-31 18:35:48 -0500

    Quote Section t&e

--- app/sections/quotes.tsx
+++ app/sections/quotes.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useState } from 'react';
+import AnimationOnScroll from "@/app/tools/AnimationOnScroll";
 
 
 export default function Quotes(){
@@ -7,14 +8,21 @@ export default function Quotes(){
     return (
         <section
             id="Quotes"
-            className="bg-black text-white font-dosis font-bold text-center lg:flex lg:flex-col lg:justify-center lg:items-center pt-10 pb-20 border-b-20 md:border-b-28 border-ugbb-red-main"
+            className="bg-black text-white font-dosis font-bold text-center lg:flex lg:flex-col lg:justify-center lg:items-center pt-10 pb-20 border-b-20 md:border-b-28 border-ugbb-red-main relative"
         >
-            <p className="italic text-lg lg:w-1/2 mx-4">
-                &#34;Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Ut et massa mi. Aliquam in hendrerit urna. Lorem ipsum dolor
-                sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna.&#34;
-            </p>
-            <p className="">- Loremizer</p>
+            <div
+                className="lg:w-1/2"
+            >
+                <AnimationOnScroll classNameInView={"animate-fade-in opactiy-100"} classNameNotInView={"opacity-0"}>
+                    <p className="italic text-lg mx-4">
+                        &#34;Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Ut et massa mi. Aliquam in hendrerit urna. Lorem ipsum dolor
+                        sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna.&#34;
+                    </p>
+                    <p className="">- Loremizer</p>
+                </AnimationOnScroll>
+            </div>
+
         </section>
     );
 }
