f004e5527d76ad1866764797f3b51672ec6548b4
Caden Epp <ccepp2412@gmail.com>
2026-04-01 00:00:01 -0500

    Classes Section t&e

--- app/components/ClassesCard.tsx
+++ app/components/ClassesCard.tsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
 import TempImage from "../../public/Equipment/LegExtensionAndCrunch.png"
-import { SquareChartGantt } from 'lucide-react';
+import {SquareChartGantt, X} from 'lucide-react';
 import { CircleArrowLeft } from 'lucide-react';
 import { CircleArrowRight } from 'lucide-react';
+import {useState} from "react";
 
 interface ClassesCardInterface {
     title: string,
 }
 
 export default function ClassesCard(props: ClassesCardInterface) {
-
+    const [moreInfoToggle, setMoreInfoToggle] = useState<boolean>(false);
 
     return (
         <div
@@ -31,7 +32,7 @@ export default function ClassesCard(props: ClassesCardInterface) {
             >
                 <div
                     id="ClassesCardInfoContainer"
-                    className="lg:w-[650px] bg-white rounded-4xl flex flex-col justify-center items-center"
+                    className="lg:w-[650px] bg-white rounded-4xl flex flex-col justify-center items-center relative"
                 >
                     <Image
                         src={TempImage}
@@ -41,12 +42,34 @@ export default function ClassesCard(props: ClassesCardInterface) {
                     <h5 className="font-sans-bartle text-2xl md:text-3xl text-ugbb-red-sec mt-6">STRENGTH</h5>
 
                     <button
-                        className="mt-9 w-6/10 flex justify-center items-center bg-ugbb-red-main py-3 rounded-4xl text-white mb-6"
+                        className="mt-9 w-6/10 flex justify-center items-center bg-ugbb-red-main py-3 rounded-4xl text-white mb-6 hover:scale-105"
+                        onClick={() => setMoreInfoToggle(true)}
                     >
-                        <span className="font-sans-bartle text-sm md:text-lg text-black text-border-white">VIEW</span>
+                        <span className="font-sans-bartle text-sm md:text-lg text-black text-border-white active:underline">VIEW</span>
                         <SquareChartGantt className="w-10 h-10 ml-2"/>
                     </button>
+
+                    {moreInfoToggle && (
+                        <div
+                            className="h-full w-full absolute z-100 flex justify-center items-center bottom-0 animate-fade-in-menu rounded-4xl"
+                        >
+                            <div
+                                className="h-full w-full bg-white rounded-4xl relative flex flex-col items-center justify-center"
+                            >
+                                <button
+                                    className="text-black absolute top-4 right-4"
+                                    onClick={() => setMoreInfoToggle(false)}
+                                >
+                                    <X className="w-[45px] h-[45px] hover:scale-110 active:border-2 active:border-black"/>
+                                </button>
+                                <h2 className="font-sans-bartle text-border-white text-4xl mb-3">INFORMATION</h2>
+                                <p className="text-white font-bold font-dosis">Lorem</p>
+                            </div>
+                        </div>
+                    )}
                 </div>
+
+
 
                 <div
                     id="ClassesControlsContainer"
@@ -56,7 +79,7 @@ export default function ClassesCard(props: ClassesCardInterface) {
                         id="ClassesControlsLeftButton"
                         className="text-black bg-white rounded-full p-1 mr-3"
                     >
-                        <CircleArrowLeft className="w-[55px] h-[55px]"/>
+                        <CircleArrowLeft className="w-[55px] h-[55px] hover:scale-110 active:border-2 active:border-white rounded-full"/>
                     </div>
 
                     <div
@@ -68,13 +91,15 @@ export default function ClassesCard(props: ClassesCardInterface) {
 
                     <div
                         id="ClassesControlsLeftButton"
-                        className="text-black bg-white rounded-full p-1 ml-3"
+                        className="text-black bg-white rounded-full p-1 ml-3 "
                     >
-                        <CircleArrowRight className="w-[55px] h-[55px]"/>
+                        <CircleArrowRight className="w-[55px] h-[55px] hover:scale-110 active:border-2 active:border-white rounded-full"/>
                     </div>
 
                 </div>
             </div>
+
+
 
 
         </div>
