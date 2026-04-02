4d43baf670f6246acc5f82bdcf062834cb3c581b
Caden Epp <ccepp2412@gmail.com>
2026-04-02 01:12:15 -0500

    Trying IOS Safari Fix Again

--- app/sections/hero.tsx
+++ app/sections/hero.tsx
@@ -2,28 +2,34 @@
 import { useState } from 'react';
 
 
-export default function Hero(){
-
+export default function Hero() {
     return (
-        <section
-            id="Hero"
-            className=""
-        >
+        <section id="Hero" className="w-full">
             <div
                 id="HeroContainer"
-                className="flex justify-center items-end md:items-center md:h-full w-full relative"
+                className="relative w-full h-svh overflow-hidden flex justify-center items-end md:items-center"
             >
-                <video className="w-full h-fit md:h-full" autoPlay={true} muted={true} loop={true} playsInline preload="auto">
-                    <source src="/video/UGBB.mp4" type="video/mp4" className="w-fit h-fit" />
+                <video
+                    className="absolute inset-0 w-full h-full object-cover"
+                    autoPlay
+                    muted
+                    loop
+                    playsInline
+                    preload="auto"
+                >
+                    <source src="/video/UGBB.mp4" type="video/mp4" />
                 </video>
+
                 <div
                     id="TitleContainer"
-                    className="flex flex-col justify-center items-center text-ugbb-red-main font-sans-bogle text-4xl md:text-9xl absolute animate-fade-in bottom-2 md:bottom-auto"
+                    className="relative z-10 flex flex-col justify-center items-center text-ugbb-red-main font-sans-bogle text-4xl md:text-9xl animate-fade-in bottom-2 md:bottom-auto"
                 >
                     <h1 className="text-border">UNDERGROUND</h1>
                     <h1 className="text-border">BARBELL</h1>
                     <hr className="border-3 md:border-6 border-white rounded-full my-2 w-3/4"/>
-                    <h3 className="text-[10px] md:text-2xl font-sans-bartle text-border">BE THE BEST - WORK HARD</h3>
+                    <h3 className="text-[10px] md:text-2xl font-sans-bartle text-border">
+                        BE THE BEST - WORK HARD
+                    </h3>
                 </div>
             </div>
         </section>
